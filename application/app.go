81b0bc4bd9f545ccb57741fdf118c13393cbc129
generalr3d <tdrdingren@gmail.com>
2024-06-13 14:50:50 +0700

    fixed bugs

--- application/app.go
+++ application/app.go
@@ -1,6 +1,8 @@
 package application
 
 import(
+	"fmt"
+	"context"
 	"net/http"
 
 )
@@ -15,5 +17,21 @@ func New() *App{
 		router:loadRoutes(),
 	}
 
-	return app
+	return app	
+}
+
+func (a *App) Start(context context.Context) error { 	//	receiver, or method that belongs to App struct
+
+
+	server := &http.Server{	//	server is a pointer var to a http server
+		Addr: ":3000",
+		Handler: a.router,	//	this.router for the App object
+	}
+
+	err := server.ListenAndServe()	//start server
+	if err != nil{
+		return fmt.Errorf("Failed to start server: %w ", err)	//	 error wrapping, wrap an error w another error around it and return that wrapped error
+	}
+
+	return nil	//	when there is no error, success
 }
--- application/routes.go
+++ application/routes.go
@@ -1,7 +1,6 @@
 package application
 
 import (
-	"context"
 	"net/http"	// includes methods to create both http clients and servers
 
 	"github.com/go-chi/chi/v5"	//	chi is a router which helps us route different URL paths
@@ -20,18 +19,3 @@ func loadRoutes() *chi.Mux {
 	return router
 }
 
-func (a *App) Start(context context.Context) error { 	//	receiver, or method that belongs to App struct
-
-
-	server := &http.Server{	//	server is a pointer var to a http server
-		Addr: ":3000",
-		Handler: a.router,	//	this.router for the App object
-	}
-
-	err := server.ListenAndServe()	//start server
-	if err != nil{
-		return fmt.Errorf("Failed to start server: %w ", err)	//	 error wrapping, wrap an error w another error around it and return that wrapped error
-	}
-
-	return nil	//	when there is no error, success
-}
